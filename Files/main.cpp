33a66be4d6fb396f8dc091ab93f83488aeeb4d1c
Max-Degtyarev <Usamaximum@mail.ru>
2022-06-13 22:03:43 +0300

    DONE

---
+++ Files/main.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<fstream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+void main()
+{
+
+	setlocale(LC_ALL, "");
+	std::ofstream fout; // Создаем поток 
+	fout.open("File.txt", std::ios_base::app); // Открываем поток
+	fout << "Hello world\n"; // Выводим поток
+	fout.close(); //Закрываем поток
+	// Потоки обязательно нужно закрывать, это также важно как удалять память
+	// Сколько раз был вызван метод open, столько же раз нужно вызвать метод close
+
+	system("notepad File.txt");
+
+
+
+
+
+
+}
