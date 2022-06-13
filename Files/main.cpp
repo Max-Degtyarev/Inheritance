32fc33307b4e73a72d94299188c9bf8872e6cfa3
Max-Degtyarev <Usamaximum@mail.ru>
2022-06-13 22:18:27 +0300

    DONE

--- Files/main.cpp
+++ Files/main.cpp
@@ -10,7 +10,8 @@ void main()
 
 	setlocale(LC_ALL, "");
 	std::ofstream fout; // Создаем поток 
-	fout.open("File.txt", std::ios_base::app); // Открываем поток
+	fout.open("File.txt"), std::ios_base::app; // Открываем поток
+	//system("REN File.txt");
 	fout << "Hello world\n"; // Выводим поток
 	fout.close(); //Закрываем поток
 	// Потоки обязательно нужно закрывать, это также важно как удалять память
